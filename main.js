f00a2df8bb553b293e5d082b44fec523f774a442
Filszu <filipszumowski05@gmail.com>
2022-03-30 16:57:12 +0200

    update

--- main.js
+++ main.js
@@ -5,12 +5,12 @@
 //https://files.muffon.endorphine.by/temp/audio/spotify/7Ie9W94M7OjPoZVV216Xus.mp3
 
 
-
 const btn = document.querySelector("#downloadTrack");
 const dwLinkBox = document.querySelector(".downloadLink");
 const songInfo = document.querySelector(".songInfo");
+const proBtn = document.querySelector("#proDownload");
 btn.addEventListener("click", download);
-
+proBtn.addEventListener("click", proDownload);
 
 
 // const muffonAPI ="https://files.muffon.endorphine.by/temp/audio/spotify/7Ie9W94M7OjPoZVV216Xus.mp3"
@@ -38,15 +38,16 @@ function download(){
 
         //simple download
         let downloadLink = `https://files.muffon.endorphine.by/temp/audio/spotify/`+trackID+`.mp3`;
-        dwLinkBox.innerHTML+=`<a href="${downloadLink}" target="_blank"> ${downloadLink}</a>`;
+        dwLinkBox.innerHTML=`<a href="${downloadLink}" target="_blank"> ${downloadLink}</a>`;
 
 
         /*Advanced download */
         songInfo.innerHTML+="loading..."
         
         //request
-        
-        fetch(muffonAPI+trackID).then(function (response){
+        console.log(muffonAPI+trackID)
+        fetch(`resources/0.json`).then(function (response){
+        // fetch(muffonAPI+trackID).then(function (response){
             return response.json();
         }).then(function (obj){
             console.log(obj);
@@ -73,13 +74,40 @@ function download(){
 
 
 function advanced(song){
+    
     console.log(JSON.stringify(song))
-    songInfo.innerHTML+=JSON.stringify(song);
+    songInfo.innerHTML=JSON.stringify(song);
+    // console.log(songInfo["track"])
+    
+
+    song.getTitle = function(){
+        return this.track.title;
+    }
+    song.getImg = function(){
+        return this.track.image.original;
+        
+    }
+    // console.log(song.track.title)
+    console.log(song.getTitle());
+    console.log(song.getImg());
+
+    songInfo.innerHTML+=`<img src="${song.getImg()}" alt=${song.getTitle()}/>`
+
+
+
+
 
     // for (const property in song) {
     //     console.log(`${property}: ${object[property]}`);
     //   }
 
+}
+
+
+function proDownload(){
+    alert("hello")
+    // window.location.assign('img/logo1.svg');
+    saveAs("aaa","aaaa.jpg")
 }
 
 // responseText: "{\"track\":{\"player_id\":\"8511d1a5-f5f5-4311-8483-6bebf25c3378\",\"source_id\":\"spotify\",\"spotify_id\":\"6yr8GiTHWvFfi4o6Q5ebdT\",\"title\":\"'Till I Collapse\",\"artists\":[{\"name\":\"Eminem\",\"spotify_id\":\"7dGJo4pcD2V6oG8kP0tJRR\"},{\"name\":\"Nate Dogg\",\"spotify_id\":\"1Oa0bMld0A3u5OTYfMzp5h\"}],\"album\":{\"source_id\":\"spotify\",\"spotify_id\":\"1ftvBBcu7jYIvXyt3JWB8S\",\"title\":\"The Eminem Show\"},\"image\":{\"original\":\"https://i.scdn.co/image/ab67616d0000b273ccdb1982626f299b3b1d3efd\",\"large\":\"https://i.scdn.co/image/ab67616d0000b273ccdb1982626f299b3b1d3efd\",\"medium\":\"https://i.scdn.co/image/ab67616d00001e02ccdb1982626f299b3b1d3efd\",\"small\":\"https://i.scdn.co/image/ab67616d00004851ccdb1982626f299b3b1d3efd\",\"extrasmall\":\"https://i.scdn.co/image/ab67616d00004851ccdb1982626f299b3b1d3efd\"},\"duration\":\"04:58\",\"duration_seconds\":298,\"audio\":{\"present\":true,\"link\":\"https://files.muffon.endorphine.by/temp/audio/spotify/6yr8GiTHWvFfi4o6Q5ebdT.mp3\",\"source_id\":\"spotify\"}}}"
