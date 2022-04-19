5d98d483bf2b63bac98fd5111cb25052896d8715
Filszu <filipszumowski05@gmail.com>
2022-04-19 13:43:22 +0200

    working website

--- script.js
+++ script.js
@@ -1,3 +1,149 @@
+let downloadLink;//just mp3
+const muffonAPI="https://muffon.endorphine.by/api/v2/spotify/tracks/"
+const standardSpotifyUrl="https://open.spotify.com/track/";
+
+const dwLinkBox = document.querySelector(".downloadLink");
+const dwLinkError = document.querySelector(".downloadLink__error");
+const songInfo = document.querySelector(".songInfo");
+const downBTN = document.querySelector(".download_btn")
+downBTN.addEventListener('click', function(){
+    download()
+    buttonTransform.start()
+});
+
+
+function download(){
+    validURL=true;
+    let spotifyURL =document.querySelector("#url").value;
+  
+
+    if(!(spotifyURL.startsWith(standardSpotifyUrl))){
+        validURL=false
+        dwLinkError.innerHTML="correct form: https://open.spotify.com/track/..."
+    }
+    else{
+        let trackID;
+        trackID = spotifyURL.indexOf("?")!=-1 ? spotifyURL.substring(standardSpotifyUrl.length,spotifyURL.indexOf("?")) : spotifyURL.substr(standardSpotifyUrl);//ucinanie adresu
+    
+            
+
+        // alert(trackID)
+        
+       
+
+        //simple download
+        downloadLink = `https://files.muffon.endorphine.by/temp/audio/spotify/`+trackID+`.mp3`;
+        dwLinkBox.innerHTML=`<a href="${downloadLink}" target="_blank"> download *.mp3</a>`;
+        dwLinkError.innerHTML="";
+
+
+        /*Advanced download */
+        song_title.innerHTML+="loading..."
+        
+        //request
+        console.log(muffonAPI+trackID)
+        // fetch(`resources/0.json`).then(function (response){
+        fetch(muffonAPI+trackID).then(function (response){
+            return response.json();
+        }).then(function (obj){
+            console.log(obj);
+            advanced(obj);
+
+            
+        }).catch(function (e){
+            console.error("You've got problem buddy");
+            console.error(e);
+        });
+        
+    }    
+}
+
+
+const song_title = document.querySelector(".song_title")
+const  song_image= document.querySelector(".song_image")
+const  song_PRO_link= document.querySelector(".song_PRO_link")
+const song_artists = document.querySelector(".song_artists")
+const song_duration = document.querySelector(".song_duration")
+
+
+
+
+
+
+function advanced(song){
+    
+    console.log(JSON.stringify(song))
+    // songInfo.innerHTML=JSON.stringify(song);
+    // console.log(songInfo["track"])
+    
+
+    song.getTitle = function(){
+        return this.track.title;
+    }
+    song.getImg = function(){
+        return this.track.image.original;
+        
+    }
+
+    song.getArtists = function(){
+        return this.track.artists[0].name;   
+    }
+    song.getDuration = function(){
+        return this.track.duration;
+    }
+
+    // console.log(song.track.title)
+    console.log(song.getTitle());
+    console.log(song.getImg());
+
+    song_image.innerHTML+=`<img src="${song.getImg()}" alt=${song.getTitle()}/>`;
+
+    song_title.innerHTML=`${song.getTitle()}`;
+
+    song_artists.innerHTML=`${song.getArtists()}`;
+    song_duration.innerHTML=`${song.getDuration()}`;
+
+
+
+
+    dwLinkBox.innerHTML+=`<br><br>Advanced download:<br><a href="${downloadLink}" target="_blank" title="download with artist and album image" onclick="download with artist and album image" download="${song.getTitle()}">${song.getTitle()}.ogg</a>`;
+
+    // proBtn.addEventListener("click", ()=>{
+    //     proDownload(song.getTitle(),downloadLink);
+    // });    
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// -----------------------animations
+
+
 const buttonTransform = KUTE.fromTo(
     '#btnStyle1', {
         path: '#btnStyle1'
@@ -11,11 +157,6 @@ const buttonTransform = KUTE.fromTo(
 
 )
 
-
-const downBTN = document.querySelector(".download_btn")
-downBTN.addEventListener('click', function(){
-    buttonTransform.start()
-});
 
 
 const blob = KUTE.fromTo(
